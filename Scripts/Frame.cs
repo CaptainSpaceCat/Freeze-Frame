4ab2b7130e82eaeaa53faea6d909e846386aa006
Connor Settle <csettle@stanford.edu>
2022-01-10 19:41:57 -0800

    Update Frame.cs

--- Scripts/Frame.cs
+++ Scripts/Frame.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +6,14 @@ public class Frame
 {
 	public static Frame destroyed = new Frame();
 	public bool notable = false;
+	//TODO
+	/* implement notability optimization
+	 * basically, each frame can decide whether or not it is "notable"
+	 * if it is, it gets saved to the timeline
+	 * if not, a pointer to the previous frame will get stored instead
+	 * this allows for using less memory to store the whole timeline
+	 * if an object has not moved or changed state in any way between frames, no need to make a new one
+	 * */
 
 	public Frame() {
 	}
